b8dd3f8969b8e66c5f62e8c0ed169710022f86e9
Robert Fox <rfox2@nd.edu>
2019-01-08 15:27:35 -0500

    More testing of hash error issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,17 +70,17 @@ namespace :deploy do
   #   end
   # end
 
-  after :published, :restart
+#   after :published, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, "cache:clear"
-      # end
-    end
-  end
-end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, "cache:clear"
+#       # end
+#     end
+#   end
+# end
 
 # before 'deploy:reload_solr_core'
 
