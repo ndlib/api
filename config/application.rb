f26b96eaf3ef5840d3e749e5026a0791200a108a
Robert Fox <rfox2@nd.edu>
2019-01-10 16:23:59 -0500

    Try including Sentry libs in application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,11 @@ require 'rails/all'
 require 'rack/jsonp'
 require File.expand_path('../../lib/hash_ostruct', __FILE__)
 
+# Sentry
+require "raven/base"
+require "raven/integrations/rails"
+require "raven/integrations/delayed_job"
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
