b205ea47e45d669633df573ce5b4b1789e25aab3
Robert Fox <rfox2@nd.edu>
2019-01-11 08:07:32 -0500

    Include sentry at top level

--- config/environments/prep.rb
+++ config/environments/prep.rb
@@ -10,7 +10,7 @@ Api::Application.configure do
   config.cache_store = :dalli_store, '127.0.0.1:11211', { compress: true, expires_in: 1.day}
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
